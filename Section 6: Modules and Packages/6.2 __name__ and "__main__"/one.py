a9ef0a8078811e5bb74afc5f64011f805b63b464
Michael Westerby <48826192+mwesterby@users.noreply.github.com>
2021-01-10 10:40:31 +0000

    Updated name and main

--- Section 6: Modules and Packages/6.2 __name__ and "__main__"/one.py
+++ Section 6: Modules and Packages/6.2 __name__ and "__main__"/one.py
@@ -3,13 +3,15 @@
 # When you call `python one.py` the `__name__` variable is assigned to "__main__"
 # __name__ = "__main__"
 
-
+# Define functions here
 def func():
     print('FUNC() IN ONE.PY')
 
 print('TOP LEVEL IN ONE.PY')
 
 if __name__ == "__main__":
+    # Run the script!
     print('ONE.PY is being run directly!')
+    # Execute functions in here
 else:
     print('ONE.PY has been imported!')
